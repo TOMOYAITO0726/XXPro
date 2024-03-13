27b02159e6433c3c251f4014d361dc6b4e9b41bd
Tomoya Ito <tomoyaito199507265724@gmail.com>
2024-03-13 18:12:43 +0900

    プロジェクト ファイルを追加します。

---
+++ CS06/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace CS06
+{
+    public partial class Form1 : Form
+    {
+        private int _value = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int GetValue()
+        {
+            int valueM = 111;
+            valueM = 123;
+            _value = 2;
+            return 1;
+        }
+    }
+}
